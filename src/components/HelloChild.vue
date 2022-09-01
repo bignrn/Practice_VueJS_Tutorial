42e2a42a4a5b2e1a02260a191962aec1f0a7b600
bignrn <raiyo2580@gmail.com>
2022-09-01 10:25:26 +0900

    チュートリアル１－取り敢えず久々なので構ってみる。
    学習内容：
    ・子コンポーネントを作成と呼び出し
    ・input要素のtext属性を使い親→子に動的にdataえお送る処理
    ・子→親にbutton要素によるincrement処理を実行する処理
    
    文献：https://blog.brainpad.co.jp/entry/2018/04/13/160000

---
+++ src/components/HelloChild.vue
@@ -0,0 +1,25 @@
+<!-- チュートリアル：https://blog.brainpad.co.jp/entry/2018/04/13/160000 -->
+<template>
+  <div class="child">
+    <h1>{{ title }}</h1>
+    <!-- ここでpropsに設定された値をそのまま表示する -->
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: "Hello Child!",
+    };
+  },
+  props: ["message"],
+};
+</script>
+
+<style>
+div .child {
+  border: 3px solid red;
+}
+</style>
---
+++ src/components/HelloChild2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="child2">
+    <h1>{{ title }}</h1>
+    <!-- ボタンが押されるとincrementCounterが実行される -->
+    <button v-on:click="incrementCounter">ボタンクリック：{{ counter }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: "Hello Child2!",
+      counter: 0,
+    };
+  },
+  methods: {
+    incrementCounter: function () {
+      this.counter += 1;
+      this.$emit("increment");
+    },
+  },
+};
+</script>
+
+<style scoped>
+div .child2 {
+  border: 5px solid pink;
+}
+</style>
